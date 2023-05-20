4e255e29831127f96e59fc5cbd08917917fd02d0
Rounak Agrawal <rounak1430@gmail.com>
2023-05-21 02:00:54 +0530

    Function to import data from student record and split it and add it to list of student are creared

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 class Class
@@ -10,9 +12,20 @@ protected:
 
 class Student : public Class
 {
-    int reg;
+
+    string reg;
     string name;
     float marks[5];
+
+public:
+    void addData(string data[])
+    {
+        reg = data[0];
+        name = data[1];
+        id = data[2];
+        for (int i = 0; i < 5; i++)
+            marks[i] = stof(data[i + 3]);
+    }
 };
 
 class Teacher : public Class
@@ -20,3 +33,39 @@ class Teacher : public Class
     int emp_id;
     string name;
 };
+
+Student students[385];
+
+void splitData(string s, int n)
+{
+    string data[8];
+    int i = 0;
+    int start, end = -1;
+    do
+    {
+        start = end + 1;
+        end = s.find(",", start);
+        data[i++] = s.substr(start, end - start);
+    } while (end != -1);
+    students[n].addData(data);
+}
+
+void getStudents()
+{
+    string line;
+    ifstream fin;
+    string lines[385];
+    fin.open("student-record.csv");
+    int i = 0;
+    while (getline(fin, line))
+        lines[i++] = line;
+    for (int j = 0; j < 385; j++)
+        splitData(lines[j], j);
+    fin.close();
+}
+
+int main()
+{
+    getStudents();
+    return 0;
+}
